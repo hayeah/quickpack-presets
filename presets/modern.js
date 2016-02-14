2e43d191fed036fc1d6d9ced42885195edbfce84
Howard Yeh <howard@metacircus.com>
2016-02-14 19:28:15 +0800

    add parameters destructuring

--- presets/modern.js
+++ presets/modern.js
@@ -2,6 +2,9 @@ module.exports = {
   plugins: [
     require("babel-plugin-transform-es2015-modules-commonjs"),
     // require("babel-plugin-transform-runtime"),
+
+    require("babel-plugin-transform-es2015-parameters"),
+
     require("babel-plugin-transform-es2015-destructuring"),
     require("babel-plugin-transform-async-to-generator"),
   ]
