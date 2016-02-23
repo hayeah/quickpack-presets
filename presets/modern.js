323b2f7888c3bd3c1cc8ef5cd537fedb142b5d6f
Howard Yeh <howard@metacircus.com>
2016-02-23 11:00:38 +0800

    add es2015-spread

--- presets/modern.js
+++ presets/modern.js
@@ -4,6 +4,7 @@ module.exports = {
     // require("babel-plugin-transform-runtime"),
 
     require("babel-plugin-transform-es2015-parameters"),
+    require("babel-plugin-transform-es2015-spread"),
 
     require("babel-plugin-transform-es2015-destructuring"),
     require("babel-plugin-transform-async-to-generator"),
