d9017370c84486eed97014ecd85235df693b66c1
Howard Yeh <howard@metacircus.com>
2016-02-24 17:50:36 +0800

    add template string

--- presets/modern.js
+++ presets/modern.js
@@ -5,6 +5,7 @@ module.exports = {
 
     require("babel-plugin-transform-es2015-parameters"),
     require("babel-plugin-transform-es2015-spread"),
+    require("babel-plugin-transform-es2015-template-literals"),
 
     require("babel-plugin-transform-es2015-destructuring"),
     require("babel-plugin-transform-async-to-generator"),
